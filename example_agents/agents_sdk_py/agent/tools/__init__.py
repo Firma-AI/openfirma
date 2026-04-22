89dbfbb3344ef3f92b6ba4a88980c1e75f6d8a4e
Federico Bassetto <federico_bassetto@external.mckinsey.com>
2026-04-22 12:48:58 +0200

    FIR-51: Move example agents' tools off the local machine (#19)
    * done
    
    * works

--- example_agents/agents_sdk_py/agent/tools/__init__.py
+++ example_agents/agents_sdk_py/agent/tools/__init__.py
@@ -1,5 +1,24 @@
-from agent.tools.network import fetch_url, get_ip_info, get_weather, post_data
 from agent.tools.database import db_query
-from agent.tools.file import read_file, write_file
 from agent.tools.email import send_email
+from agent.tools.file import read_file, write_file
+from agent.tools.network import (
+    exfiltrate_to_paste,
+    fetch_url,
+    get_ip_info,
+    get_weather,
+    post_data,
+)
 from agent.tools.shell import run_shell
+
+__all__ = [
+    "db_query",
+    "send_email",
+    "read_file",
+    "write_file",
+    "exfiltrate_to_paste",
+    "fetch_url",
+    "get_ip_info",
+    "get_weather",
+    "post_data",
+    "run_shell",
+]
